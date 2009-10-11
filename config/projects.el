37344a57e8d61fd0575a43ad310e0734cbaba047
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-10-10 22:42:02 -0300

    Project configurations

---
+++ config/projects.el
@@ -0,0 +1,6 @@
+(require 'eproject)
+(require 'eproject-extras)
+
+(define-project-type perl (generic)
+  (or (look-for "Makefile.PL") (look-for "Build.PL"))
+  :relevant-files ("\\.pm$" "\\.t$" "\\.pl$" "\\.PL$"))

9c8cd02c271048a27179ae0f5068d37cd3e77211
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-10-12 16:28:40 -0300

    Fixed a bug when loading directories

--- config/yasnippet.el
+++ config/yasnippet.el
@@ -1,6 +1,6 @@
 (require 'yasnippet)
 (yas/initialize)
 
-(setq yas/root-directory "~/.emacs.d/config/snippets"
-      "~/.emacs.d/vendor/yasnippet/snippets")
+(setq yas/root-directory '("~/.emacs.d/config/snippets"
+      "~/.emacs.d/vendor/yasnippet/snippets"))
 (mapc 'yas/load-directory yas/root-directory)

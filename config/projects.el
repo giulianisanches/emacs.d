bf80e686984ff8cb277c8b7e3c284f9156122451
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-10-12 16:20:18 -0300

    Added tt extension to perl projects

--- config/projects.el
+++ config/projects.el
@@ -3,4 +3,4 @@
 
 (define-project-type perl (generic)
   (or (look-for "Makefile.PL") (look-for "Build.PL"))
-  :relevant-files ("\\.pm$" "\\.t$" "\\.pl$" "\\.PL$"))
+  :relevant-files ("\\.pm$" "\\.t$" "\\.pl$" "\\.PL$" "\\.tt2?$"))

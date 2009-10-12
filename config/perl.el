b4162476895fdd50f07def6990911fcb8af538dc
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-10-12 17:52:38 -0300

    Seems that the bug is'n in perltidy or perlcritic.el.

--- config/perl.el
+++ config/perl.el
@@ -1,7 +1,7 @@
 (defalias 'perl-mode 'cperl-mode)
 
-;; (require 'perltidy)
-;; (require 'perlcritic)
+(require 'perltidy)
+(require 'perlcritic)
 
 (setq cperl-close-paren-offset -4
       cperl-continued-statement-offset 4

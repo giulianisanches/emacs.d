3bc7e75e27aaca7480046e1b87c0ae7c9cf5ed66
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-10-14 22:28:07 -0300

    Changed the theme

--- config/theme.el
+++ config/theme.el
@@ -1,6 +1,7 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
+(load "themes/color-theme-tango")
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-charcoal-black)))
+     (color-theme-tango)))

6ec275c1488665cb231541fa0b4939915d244035
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-10-15 21:14:34 -0300

    Charcoal black is back for a while

--- config/theme.el
+++ config/theme.el
@@ -1,7 +1,6 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
-(load "themes/color-theme-tango")
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-tango)))
+     (color-theme-charcoal-black)))

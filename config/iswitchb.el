c05a0a8451b1f5a8d63f4686d591d50567a6f971
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-11-03 23:34:05 -0200

    Configurações iniciais para o iswitchb

---
+++ config/iswitchb.el
@@ -0,0 +1,6 @@
+(require 'iswitchb)
+(iswitchb-mode 1)
+
+(add-to-list 'iswitchb-buffer-ignore "*Messages*")
+(add-to-list 'iswitchb-buffer-ignore "*scratch")
+(add-to-list 'iswitchb-buffer-ignore "*Completions")

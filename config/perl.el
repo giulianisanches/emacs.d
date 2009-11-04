a8f31aad90d6ff9eacc0b751458cb470234f418a
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-11-03 23:47:07 -0200

    Associada a extensão .t ao cperl-mode

--- config/perl.el
+++ config/perl.el
@@ -3,6 +3,8 @@
 (require 'perltidy)
 (require 'perlcritic)
 
+(add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
+
 (setq cperl-close-paren-offset -4
       cperl-continued-statement-offset 4
       cperl-indent-parens-as-block t

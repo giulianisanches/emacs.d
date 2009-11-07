dc91edd884a874ff0b07b6735b241b6ab6015cb0
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-11-07 20:27:09 -0200

    Adicionado mais um tipo de buffer ao ignore list

--- config/iswitchb.el
+++ config/iswitchb.el
@@ -2,5 +2,6 @@
 (iswitchb-mode 1)
 
 (add-to-list 'iswitchb-buffer-ignore "*Messages*")
-(add-to-list 'iswitchb-buffer-ignore "*scratch")
-(add-to-list 'iswitchb-buffer-ignore "*Completions")
+(add-to-list 'iswitchb-buffer-ignore "*scratch*")
+(add-to-list 'iswitchb-buffer-ignore "*Completions*")
+(add-to-list 'iswitchb-buffer-ignore "*Buffe List*")

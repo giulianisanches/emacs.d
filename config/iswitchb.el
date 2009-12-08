1596206d11731163a7a5fe49217aefa344958a44
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2009-12-08 21:54:17 -0200

    simplificada a lista de exclusões

--- config/iswitchb.el
+++ config/iswitchb.el
@@ -1,9 +1,4 @@
 (require 'iswitchb)
 (iswitchb-mode 1)
 
-(add-to-list 'iswitchb-buffer-ignore "*Messages*")
-(add-to-list 'iswitchb-buffer-ignore "*scratch*")
-(add-to-list 'iswitchb-buffer-ignore "*Completions*")
-(add-to-list 'iswitchb-buffer-ignore "*Buffe List*")
-(add-to-list 'iswitchb-buffer-ignore "*Help*")
-(add-to-list 'iswitchb-buffer-ignore "*Backtrace*")
+(add-to-list 'iswitchb-buffer-ignore "^\\*")

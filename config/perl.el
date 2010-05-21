38d860a3fbf05e97e37eaa07a303020474aedf4d
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2010-05-20 22:22:13 -0300

    O emacs se perdia com a syntaxe ao chamar o perltidy. É necessário configura o cperl-mode e o perl-mode para o perltidy funcionar corretamente

--- config/perl.el
+++ config/perl.el
@@ -5,13 +5,25 @@
 
 (eval-after-load "cperl-mode"
   '(add-hook 'cperl-mode-hook 'perlcritic-mode))
+(eval-after-load "perl-mode"
+  '(add-hook 'cperl-mode-hook 'perlcritic-mode))
 
 ;; ;; perltidy-mode
 (autoload 'perltidy "perltidy-mode" nil t)
 (autoload 'perltidy-mode "perltidy-mode" nil t)
+(autoload 'perltidy-unless-readonly-mode "perltidy-mode" nil t)
 
 (eval-after-load "cperl-mode"
-  '(add-hook 'cperl-mode-hook 'perltidy-mode))
+  '(add-hook 'cperl-mode-hook 'perltidy-unless-readonly-mode))
+(eval-after-load "perl-mode"
+  '(add-hook 'cperl-mode-hook 'perltidy-unless-readonly-mode))
+
+;; pod-mode
+(require 'pod-mode)
+(setq auto-mode-alist
+      (append auto-mode-alist
+              '(("\\.pod$" . pod-mode))))
+(add-hook 'pod-mode-hook 'font-lock-mode)
 
 ;; cperl-mode
 (load "cperl-mode")
@@ -23,10 +35,3 @@
       cperl-continued-statement-offset 4
       cperl-indent-parens-as-block t
       cperl-tab-always-indent nil)
-
-;; pod-mode
-(require 'pod-mode)
-(setq auto-mode-alist
-      (append auto-mode-alist
-              '(("\\.pod$" . pod-mode))))
-(add-hook 'pod-mode-hook 'font-lock-mode)

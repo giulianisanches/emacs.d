6901f354065a7c60fb5cc1d5c0be17e3c69f08e8
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2012-07-08 22:27:10 -0300

    I`m not using perl at all, so no need to have all these packages and configurations.

--- vendor/template-toolkit-mode.el
+++
@@ -1,8 +0,0 @@
-;; Create a template toolkit mode derived from html-mode
-;; reference: http://blog.alexn.org/2008/11/emacs-textmate-like-snippets.html
-
-;; specifies that this script provide an emacs module
-(provide 'template-toolkit-mode)
-
-;; defines a derived editing mode, inheriting from html-mode
-(define-derived-mode template-toolkit-mode html-mode "Template Toolkit" nil)

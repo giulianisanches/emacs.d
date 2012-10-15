3516d25f1091ccc1abd21870d02fd3e3ba64eb86
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2012-10-15 20:01:51 -0300

    Clojure project definition

--- config/projects.el
+++ config/projects.el
@@ -1,3 +1,7 @@
 (define-project-type python (generic)
   (look-for "__init__.py")
   :relevant-files ("\\.py$"))
+
+(define-project-type clojure (generic)
+  (look-for "project.clj")
+  :relevant-files ("\\.clj$"))

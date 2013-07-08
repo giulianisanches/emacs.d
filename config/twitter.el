f484981ae713079e0bbed7d0578bb38ca2b811da
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2013-07-08 19:33:55 -0300

    Atualização e ajustes no twittering-mode

--- config/twitter.el
+++ config/twitter.el
@@ -1,7 +1,15 @@
 (require 'twittering-mode)
 
-(setq twittering-use-master-password t)
-(setq twittering-timer-interval 300)
+(setq twittering-use-master-password t
+      twittering-timer-interval 300
+      twittering-url-show-status nil
+      twittering-icon-mode nil
+      twittering-private-info-file "~/Google Drive/Keys/twitter.gpg"
+      twittering-initial-timeline-spec-string
+      '(":home" ":replies" ":direct_messages")
+      twittering-status-format "%s (%S)\n%FILL[ ]{%T}\n %FACE[glyphless-char]{%@ from %f%L%r%R}\n")
+
+(set-face-attribute 'twittering-username-face nil :underline nil)
 
 ;;(setq twittering-tinyurl-service 'bit.ly)
 ;;(setq twittering-bitly-login "YOURbitlyUsername")

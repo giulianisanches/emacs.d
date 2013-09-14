018683da107e7960e1b1c9237bd902b23429f990
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2013-09-14 12:35:48 -0300

    bug fixes

--- config/indentation.el
+++ config/indentation.el
@@ -1,4 +1,3 @@
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-stop-list (number-sequence 4 120 4))
-(setq nxml-child-indent 4)
--- init.el
+++ init.el
@@ -9,18 +9,71 @@
 
 (setq inhibit-startup-message t)
 
+
+(setq default-frame-alist
+      '((top . 0) (left . 250)
+        (width . 80) (height . 36)))
+
+(setq initial-scratch-message nil)
+(setq make-backup-files nil)
+(setq scroll-step 1)
+(setq major-mode 'text-mode)
+(setq kill-whole-line t)
+(setq default-truncate-lines t)
+(setq truncate-partial-width-windows nil)
+
+(if (eq system-type 'windows-nt)
+    (set-face-attribute 'default nil :font "Consolas-11"))
+
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(prefer-coding-system 'utf-8)
+
+(line-number-mode 1)
+(column-number-mode 1)
+
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/vendor")
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/extra-pkg")
 
 (when (or (eq system-type 'darwin) (eq system-type 'gnu/linux))
   (add-to-list 'exec-path "/usr/local/bin")
   (add-to-list 'exec-path "/usr/local/share/python"))
 
-(load "config/global")
+(when (or (eq system-type 'darwin) (eq system-type 'gnu/linux))
+  (require 'exec-path-from-shell)
+  (exec-path-from-shell-initialize))
+
+(require 'autopair)
+(autopair-global-mode)
+
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(require 'iswitchb)
+(iswitchb-mode 1)
+(add-to-list 'iswitchb-buffer-ignore "^\\*")
+
+(require 'uniquify)
+
+(require 'epa-file)
+(epa-file-enable)
+
+(require 'paredit)
+(require 'org)
+
 (load "config/indentation")
 (load "config/hooks")
 (load "config/defuns")

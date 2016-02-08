a8f411e4dbb8c42f42b13e43b36ef4a5724cd44c
Giuliani D. Sanches <giulianisanches@users.noreply.github.com>
2016-02-08 17:58:16 -0200

    Removed some unused code due to emacs already proivde them

--- config/emacs/keymaps.el
+++ config/emacs/keymaps.el
@@ -1,13 +1,7 @@
 (global-set-key (kbd "RET") 'newline-and-indent)
 
-;; Look at config/defuns.el
-(global-set-key [C-S-right] 'shift-right)
-(global-set-key [C-S-left] 'shift-left)
-
 ;; Recent files
 (global-set-key (kbd "C-c f") 'recentf-open-files)
 (global-set-key (kbd "C-c r") 'revert-buffer)
 
-;; See config/defuns.el for details
-(global-set-key (kbd "M-;") 'comment-dwin-line)
 (global-set-key (kbd "<C-tab>") 'buffer-menu)

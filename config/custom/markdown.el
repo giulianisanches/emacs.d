de542d68703acb0b0860b9fe38960456876fe7bc
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2022-08-26 11:55:36 -0300

    Add hook to truncate lines for this mode
    A minor change was to move a configuration from
    init.el to here

--- config/custom/markdown.el
+++ config/custom/markdown.el
@@ -3,3 +3,6 @@
 (add-to-list 'auto-mode-alist '("\\.text|\\.mdw?\\'" . markdown-mode))
 (setq markdown-hr-string "----")
 (setq markdown-italic-underscore t)
+(setq markdown-command "pandoc")
+
+(add-hook 'markdown-mode-hook (lambda () (setq truncate-lines t)))

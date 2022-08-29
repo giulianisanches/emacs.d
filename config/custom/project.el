7da04ae1add86191f604072d58deefadc29de0d4
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2022-08-29 10:29:03 -0300

    Add a hook to auto activate python virtualenv
    This is an attempt to mimic vscode behaviour of
    loading a virtualenv from .venv directory whithin
    the project, if it exists.

--- config/custom/project.el
+++ config/custom/project.el
@@ -9,3 +9,15 @@
 (setq projectile-project-search-path '(("~/dev/src/" . 3)))
 
 (projectile-mode +1)
+
+(defun pyvenv-autoload ()
+  "Automatically activates pyvenv version if .venv directory exists."
+  (f-traverse-upwards
+   (lambda (path)
+     (let ((venv-path (f-expand ".venv" path)))
+       (if (f-exists? venv-path)
+           (progn
+             (pyvenv-workon venv-path))
+             t)))))
+
+(add-hook 'projectile-after-switch-project-hook #'pyvenv-autoload)

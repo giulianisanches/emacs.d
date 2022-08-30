ee691dd2402bce1c46f747f7eb8d93d90b390852
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2022-08-30 09:00:53 -0300

    Remove a extra keymap from projectile conf

--- config/custom/project.el
+++ config/custom/project.el
@@ -9,7 +9,6 @@
 
 ;; Recommended keymap prefix on macOS
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
 ;; Recommended keymap prefix on Windows/Linux
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 

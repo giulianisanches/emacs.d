2fecb6908c96de8990268546178b46653d2544f5
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2022-09-08 10:37:11 -0300

    Add some lsp-mode configuration

---
+++ config/custom/lsp.el
@@ -0,0 +1,16 @@
+;;; lsp.el --- Configuration for lsp-mode
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(require 'lsp)
+
+(setq lsp-ui-doc-show-with-cursor nil)
+(setq lsp-ui-sideline-enable nil)
+
+(setq lsp-lens-enable nil)
+(setq lsp-headerline-breadcrumb-enable nil)
+(setq lsp-signature-render-documentation nil)
+
+;;; lsp.el ends here
